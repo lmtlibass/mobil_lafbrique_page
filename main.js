8a63b28d2b48e0743b5227827f66a779f5576280
thiam <lmtlibasse@saloumpharma.com>
2021-11-20 13:28:53 +0000

    initialiser git page

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function Menu(){
+    var links = document.querySelector(".link");
+        if(links.style.display === "none"){
+            links.style.display = "block";
+        }else {
+            links.style.display = "none";
+        }
+}
